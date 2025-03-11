b4a993a312a29d9f02770551c336f4162eb84fa1
NgoVanTrung2004 <trung.nv.64cntt@ntu.edu.vn>
2025-03-11 22:09:38 +0700

    Create MainActivity.java

---
+++ Ex7_IntentLogin/app/src/main/java/NVT/ex7_intentlogin/MainActivity.java
@@ -0,0 +1,40 @@
+package NVT.ex7_intentlogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // tìm đk
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        // sự kiện
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // lệnh xử lý ở đây
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin); // Đã sửa lại đúng phương thức
+            }
+        }); // Đóng ngoặc tại đây
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
