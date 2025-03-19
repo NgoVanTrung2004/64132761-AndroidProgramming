350b2e4a2a04ac20c8a8a0048e0c9da1bfe24d17
NgoVanTrung2004 <trung.nv.64cntt@ntu.edu.vn>
2025-03-19 09:08:15 +0700

    Create MainActivity.java

---
+++ ThietKe_XML/app/src/main/java/com/example/thietke_xml/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.thietke_xml;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    CardView clothingCard;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        clothingCard = findViewById(R.id.clothingCard);
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ClothingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
