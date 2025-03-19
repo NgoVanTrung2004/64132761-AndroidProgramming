e6744d21fc69afe9521e90698a4ea3f5da9ad3b5
NgoVanTrung2004 <trung.nv.64cntt@ntu.edu.vn>
2025-03-19 07:56:19 +0700

    Create MainActivity.java

---
+++ vidutrenlop/app/src/main/java/com/example/vidutrenlop/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.vidutrenlop;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+Button btnOK;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Chào mừng", Toast.LENGTH_SHORT ).show();
+            }
+        });
+    }
+}
